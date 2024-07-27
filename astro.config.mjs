06fdf5b7d33fe821f795cac8a3e7c9d5737185b4
Degraft Frimpong <frimpsup@gmail.com>
2024-07-26 22:50:01 -0400

    build success

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://compa.so",
+	site: "https://gctuvc.com",
 	integrations: [UnoCSS({ injectReset: true })],
 });
